c6c7ab048d99c7d2cffea92387776c044064a8de
Zanidean` <`AZanidean@mhc.ab.ca`>
2017-06-26 13:23:11 -0600

    Update foip()

--- R/foip.R
+++ R/foip.R
@@ -2,14 +2,13 @@
 #'@description Really can be useful for protecting privacy, but have to be aware of how to use it safely.
 #'@param column column to subset on
 #'@param dataframe data to be foiped
-
 #'
 #'@examples foip(program, survey)
 #'
 #'@export foip
 foip <- function(column, dataframe){
   df <- dataframe
-  dfsum <- dplyr::select_(dataframe, as.name(column))
+  dfsum <- select(dataframe, as.name(column))
   dfsum <- as.data.frame(table(dfsum[[column]]), responseName="Count")
   dfsum <- dfsum[dfsum$Count >=5,]
   df <- dataframe[df[[column]] %in% dfsum$Var1,]
